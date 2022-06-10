7caa9354d7ceb871fd86b42b880b5ba0789e2784
Geekureuil <derrey.xavier@gmail.com>
2022-06-10 07:09:37 +0200

    deplacement index.php dans /public

--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,7 @@
 
 $factory = new VehiculeFactory();
 
-$fichierVehicules = json_decode(file_get_contents("vehicules.json"));
+$fichierVehicules = json_decode(file_get_contents("../vehicules.json"));
 
 foreach ($fichierVehicules as $dataVehicule) {
     $vehicule = $factory->makeVehicule($dataVehicule);
